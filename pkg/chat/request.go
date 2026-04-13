d07a6f38873d40ed41f96bbba562797410244db6
DESKTOP-NBQ48LN\yrj <alpiny@qq.com>
2026-04-13 15:11:52 +0800

    更新注释

--- pkg/chat/request.go
+++ pkg/chat/request.go
@@ -10,13 +10,25 @@ type Request struct {
 	Meta     map[string]any   `json:"meta,omitzero"`
 	Messages []RequestMessage `json:"messages"`
 	Tools    []Tool           `json:"tools,omitzero"`
-	// 随机性
+
+	// 采样温度
+	// 控制模型生成文本的多样性
+	// temperature越高，生成的文本更多样，反之，生成的文本更确定
+	// 取值范围： [0, 2)
 	// 0.001:最保守 1:有创造性 1.2:非常随机(可能胡说)
 	Temperature float64 `json:"temperature,omitempty"`
-	// 核采样
+	// 核采样的概率阈值
+	// 控制模型生成文本的多样性
+	// top_p越高，生成的文本更多样。反之，生成的文本更确定
+	// 取值范围：（0,1.0]
 	// 0.5:很保守 0.9:只选高概率词 1.0:不限制（最随机）
 	TopP float64 `json:"top_p,omitempty"`
 	// 存在惩罚
+	// 控制模型生成文本时的内容重复度
+	// 在创意写作或头脑风暴等需要多样性、趣味性或创造力的场景中，建议调高该值；
+	// 在技术文档或正式文本等强调一致性与术语准确性的场景中，建议调低该值。
+	// 取值范围：[-2.0, 2.0]
+	// 正值降低重复度，负值增加重复度
 	// 0.001:多说已存在的词 1:多用新词
 	PresencePenalty float64 `json:"presence_penalty,omitempty"`
 	// 频率惩罚
